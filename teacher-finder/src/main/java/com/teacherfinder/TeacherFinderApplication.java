416bb0269e51dbd65daf1727760049f26846422b
Kevin Stif Sánchez Urbina <67239585+kevinstif@users.noreply.github.com>
2023-05-22 17:31:43 -0500

    first commit

---
+++ teacher-finder/src/main/java/com/teacherfinder/TeacherFinderApplication.java
@@ -0,0 +1,13 @@
+package com.teacherfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherFinderApplication.class, args);
+	}
+
+}
---
+++ teacher-finder/src/test/java/com/teacherfinder/TeacherFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teacherfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
