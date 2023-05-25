ef51775f7ae27769a60161e4cbbe39b5deef82da
Markollantes2307 <72058326+Markollantes2307@users.noreply.github.com>
2023-05-25 17:19:24 -0500

    feat: B.C. Assessment Domain

---
+++ Assessment/Domain/AnswerOption.cs
@@ -0,0 +1,7 @@
+namespace DefaultNamespace;
+
+public class AnswerOption
+{
+    public string answer { get; set; }
+    public bool isCorrect { get; set; }
+}
---
+++ Assessment/Domain/Question.cs
@@ -0,0 +1,8 @@
+namespace DefaultNamespace;
+
+public class Question
+{
+    public long id { get; set; }
+    public string statements { get; set; }
+    public float points { get; set; }
+}
