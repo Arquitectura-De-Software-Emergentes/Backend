fe94e7f806b867441aca1d22cf8b3c4d65154fa2
Jesús Pacheco <jesuspacheco0219@gmail.com>
2023-05-27 23:05:34 -0500

    add Enums to BC Offers

---
+++ src/main/java/com/teacherfinder/Offers/domain/model/Enum/Availability.java
@@ -0,0 +1,6 @@
+package com.teacherfinder.Offers.domain.model.Enum;
+
+public enum Availability {
+    available,
+    unavailable
+}
---
+++ src/main/java/com/teacherfinder/Offers/domain/model/Enum/Type.java
@@ -0,0 +1,6 @@
+package com.teacherfinder.Offers.domain.model.Enum;
+
+public enum Type {
+    PartTime,
+    Fulltime
+}
