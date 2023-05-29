79459d47fcb5ec4dee7e57e18cde44b6e9a5ed63
Kevin Stif Sánchez Urbina <67239585+kevinstif@users.noreply.github.com>
2023-05-29 14:14:56 -0500

    refactor(InstitutionProfile): :fire: remove a codeline unused

--- src/main/java/com/teacherfinder/offers/domain/model/valueObject/Course.java
+++ src/main/java/com/teacherfinder/offers/domain/model/valueObject/Course.java
@@ -2,6 +2,12 @@ package com.teacherfinder.offers.domain.model.valueObject;
 
 import javax.persistence.Embeddable;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
 @Embeddable
 public class Course {
     private String course;
