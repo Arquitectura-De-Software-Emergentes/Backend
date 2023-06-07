ee23ffe10fe7a36dc5a92ca93e36b9c973ad65a5
Kevin Stif Sánchez Urbina <67239585+kevinstif@users.noreply.github.com>
2023-06-07 10:20:32 -0500

    feat: :sparkles: clone applicant job experience information

--- src/main/java/com/teacherfinder/applications/application/service/ApplicationServiceImpl.java
+++ src/main/java/com/teacherfinder/applications/application/service/ApplicationServiceImpl.java
@@ -1,11 +1,13 @@
 package com.teacherfinder.applications.application.service;
 
+import java.util.List;
 import java.util.Set;
 
 import javax.transaction.Transactional;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 
+import com.teacherfinder.applications.domain.model.entity.ApplicationJobExperienceInformation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,7 @@ import com.teacherfinder.applications.domain.factory.ApplicationFactory;
 import com.teacherfinder.applications.domain.model.aggregate.Application;
 import com.teacherfinder.applications.domain.model.entity.ApplicationApplicantProfile;
 import com.teacherfinder.applications.domain.repository.ApplicationApplicantProfileRepository;
+import com.teacherfinder.applications.domain.repository.ApplicationJobExperienceInformationRepository;
 import com.teacherfinder.applications.domain.repository.ApplicationRepository;
 import com.teacherfinder.applications.domain.service.ApplicationService;
 import com.teacherfinder.profile.api.internal.ProfileFacade;
@@ -30,18 +33,19 @@ public class ApplicationServiceImpl implements ApplicationService{
     private static final String APPLICATION = "application";
     private final ApplicationRepository applicationRepository;
     private final ApplicationApplicantProfileRepository profileRepository;
+    private final ApplicationJobExperienceInformationRepository experienceRepository;
     private final ApplicationFactory applicationFactory;
     private final ProfileFacade profileFacade;
     private final Validator validator;
 
-    
-
     public ApplicationServiceImpl(ApplicationApplicantProfileMapper profileMapper,
             ApplicationRepository applicationRepository, ApplicationApplicantProfileRepository profileRepository,
-            ApplicationFactory applicationFactory, ProfileFacade profileFacade, Validator validator) {
+            ApplicationJobExperienceInformationRepository experienceRepository, ApplicationFactory applicationFactory,
+            ProfileFacade profileFacade, Validator validator) {
         this.profileMapper = profileMapper;
         this.applicationRepository = applicationRepository;
         this.profileRepository = profileRepository;
+        this.experienceRepository = experienceRepository;
         this.applicationFactory = applicationFactory;
         this.profileFacade = profileFacade;
         this.validator = validator;
@@ -56,6 +60,7 @@ public class ApplicationServiceImpl implements ApplicationService{
         ApplicationApplicantProfile profile = profileMapper.toApplicationApplicantProfile(profileFacade.getApplicantProfile(applyResource.getApplicantId()));
 
         profile = saveProfile(profile);
+        saveJobExperience(profile.getJobExperienceInformations(), profile);
         
         Application application = applicationFactory.createApplication(applyResource.getApplicantId(), applyResource.getJobOfferId(), DEFAULT_STATUS, profile);
 
@@ -72,6 +77,15 @@ public class ApplicationServiceImpl implements ApplicationService{
 
     private ApplicationApplicantProfile saveProfile(ApplicationApplicantProfile profile){
         return profileRepository.save(profile);
+    }
+
+    private void saveJobExperience(List<ApplicationJobExperienceInformation> experiences, ApplicationApplicantProfile profile){
+
+        for (ApplicationJobExperienceInformation experience : experiences) {
+            experience.setApplicantProfile(profile);
+            experienceRepository.save(experience);
+        }
+
     }
     
 }
