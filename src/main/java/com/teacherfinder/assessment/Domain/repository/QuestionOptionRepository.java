39cf2ac6b806c1c4c8a8c02bb0adc3bb9b28a297
Kevin Stif Sánchez Urbina <67239585+kevinstif@users.noreply.github.com>
2023-06-17 00:35:23 -0500

    refactor(Question): :pencil2: change data type options

--- src/main/java/com/teacherfinder/assessment/Domain/repository/QuestionOptionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.teacherfinder.assessment.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.teacherfinder.assessment.domain.model.entity.QuestionOption;
-
-public interface QuestionOptionRepository extends JpaRepository<QuestionOption, Long>{
-    
-}
--- src/main/java/com/teacherfinder/assessment/domain/model/entity/Question.java
+++ src/main/java/com/teacherfinder/assessment/domain/model/entity/Question.java
@@ -1,15 +1,11 @@
 package com.teacherfinder.assessment.domain.model.entity;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
@@ -34,9 +30,16 @@ public class Question {
     @NotBlank(message = "The statement is required")
     private String statement;
 
-    @OneToMany(mappedBy = "question", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    private List<QuestionOption> options;
-
     @ManyToOne(fetch = FetchType.LAZY)
     private TestActivity test;
+
+    private String option1;
+
+    private String option2;
+
+    private String option3;
+
+    private String option4;
+
+    private String reponse;
 }
