163fac3eb172a0cbf2ff60e8f6965f50dbb8c209
Kevin Stif Sánchez Urbina <67239585+kevinstif@users.noreply.github.com>
2023-06-17 23:49:05 -0500

    feat(AssesmentServiceImpl): :sparkles: Add options function

--- src/main/java/com/teacherfinder/assessment/application/service/AssesmentServiceImpl.java
+++ src/main/java/com/teacherfinder/assessment/application/service/AssesmentServiceImpl.java
@@ -1,7 +1,9 @@
 package com.teacherfinder.assessment.application.service;
 
+import java.util.List;
 import java.util.Set;
 
+import javax.transaction.Transactional;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 
@@ -10,7 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.teacherfinder.assessment.domain.model.entity.Question;
+import com.teacherfinder.assessment.domain.model.entity.QuestionOption;
 import com.teacherfinder.assessment.domain.model.entity.TestActivity;
+import com.teacherfinder.assessment.domain.repository.QuestionOptionRepository;
 import com.teacherfinder.assessment.domain.repository.QuestionRepository;
 import com.teacherfinder.assessment.domain.repository.TestActivityRepository;
 import com.teacherfinder.assessment.domain.service.AssesmentService;
@@ -18,45 +22,63 @@ import com.teacherfinder.shared.exception.ResourceNotFoundException;
 import com.teacherfinder.shared.exception.ResourceValidationException;
 
 @Service
-public class AssesmentServiceImpl implements AssesmentService{
+public class AssesmentServiceImpl implements AssesmentService {
 
     private static final String TEST = "test";
     private static final String QUESTION = "question";
+    private static final String OPTION = "option";
     private final TestActivityRepository testRepository;
     private final QuestionRepository questionRepository;
+    private final QuestionOptionRepository optionRepository;
     private final Validator validator;
 
     public AssesmentServiceImpl(TestActivityRepository testRepository, QuestionRepository questionRepository,
-            Validator validator) {
+            QuestionOptionRepository optionRepository, Validator validator) {
         this.testRepository = testRepository;
         this.questionRepository = questionRepository;
+        this.optionRepository = optionRepository;
         this.validator = validator;
     }
 
     @Override
     public ResponseEntity<String> createTest(TestActivity test) {
         Set<ConstraintViolation<TestActivity>> violations = validator.validate(test);
-        if(!violations.isEmpty())
+        if (!violations.isEmpty())
             throw new ResourceValidationException(TEST, violations);
-            
+
         testRepository.save(test);
-        
+
         return new ResponseEntity<String>("Test was successfully saved", HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<String> addQuestion(Long testId,Question question) {
+    @Transactional
+    public ResponseEntity<String> addQuestion(Long testId, Question question) {
         Set<ConstraintViolation<Question>> violations = validator.validate(question);
 
-        if(!violations.isEmpty())
+        if (!violations.isEmpty())
             throw new ResourceValidationException(QUESTION, violations);
 
         TestActivity test = testRepository.findById(testId)
-            .orElseThrow(()-> new ResourceNotFoundException(QUESTION, testId));
+                .orElseThrow(() -> new ResourceNotFoundException(QUESTION, testId));
 
-        questionRepository.save(question.withTest(test));
+        Question result = questionRepository.save(question.withTest(test));
+
+        addOptions(question.getOptions(), result);
 
         return new ResponseEntity<String>("Question was successfully saved", HttpStatus.OK);
     }
-    
+
+    private void addOptions(List<QuestionOption> options, Question question) {
+        
+        for (QuestionOption option : options) {
+            Set<ConstraintViolation<QuestionOption>> violations = validator.validate(option);
+
+            if (!violations.isEmpty())
+                throw new ResourceValidationException(OPTION, violations);
+
+            optionRepository.save(option.withQuestion(question));
+        }
+    }
+
 }
