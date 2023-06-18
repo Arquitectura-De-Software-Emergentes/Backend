b18944eec31210292eebccc6f671c2978e398fec
Kevin Stif Sánchez Urbina <67239585+kevinstif@users.noreply.github.com>
2023-06-18 00:57:16 -0500

    feat(AssesmentController): :sparkles: Add enpoint For Get Test

--- src/main/java/com/teacherfinder/assessment/api/rest/AssesmentController.java
+++ src/main/java/com/teacherfinder/assessment/api/rest/AssesmentController.java
@@ -3,6 +3,7 @@ package com.teacherfinder.assessment.api.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.teacherfinder.assessment.application.dto.CreateQuestionResource;
 import com.teacherfinder.assessment.application.dto.CreateTestResource;
+import com.teacherfinder.assessment.application.dto.TestResource;
 import com.teacherfinder.assessment.application.mapper.QuestionMapper;
 import com.teacherfinder.assessment.application.mapper.TestActivityMapper;
 import com.teacherfinder.assessment.domain.service.AssesmentService;
@@ -45,4 +47,8 @@ public class AssesmentController {
         return service.addQuestion(testId, questionMapper.toModel(resource));
     }
     
+    @GetMapping("/tests/{testId}")
+    public TestResource GetTestById(@RequestParam("testId") Long testId){
+        return testMapper.toResource(service.GetTestById(testId));
+    }
 }
