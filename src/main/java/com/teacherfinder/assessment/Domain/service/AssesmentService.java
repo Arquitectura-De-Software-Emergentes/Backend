e785bf56af8faea2c88f7e559bf68ce213715a35
Kevin Stif Sánchez Urbina <67239585+kevinstif@users.noreply.github.com>
2023-06-20 11:57:15 -0500

    feat(AssessmentService): :sparkles: calculate test result

--- src/main/java/com/teacherfinder/assessment/Domain/service/AssesmentService.java
+++ src/main/java/com/teacherfinder/assessment/Domain/service/AssesmentService.java
@@ -1,12 +1,20 @@
 package com.teacherfinder.assessment.domain.service;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 
+import com.teacherfinder.assessment.domain.model.aggregate.Assessment;
 import com.teacherfinder.assessment.domain.model.entity.Question;
 import com.teacherfinder.assessment.domain.model.entity.TestActivity;
+import com.teacherfinder.assessment.domain.model.entity.TestResult;
 
 public interface AssesmentService {
-    TestActivity createTest(TestActivity test);
-    ResponseEntity<String> addQuestion(Long testId, Question question);
-    TestActivity GetTestById(Long testId);
+    Assessment createAssessment(Assessment assessment);
+    Assessment getAssessmentByJobOfferId(Long jobOfferId);
+    TestActivity addTest(Long assessmentId,TestActivity test);
+    ResponseEntity<String> addQuestion(Long assessmentId, Question question);
+    TestActivity GetTestByAssessmentId(Long assessmentId);
+    TestResult calculateScore(Long assessmentId, Long applicantId,List<Question> questions);
+    TestResult getResultByTestResultId(Long assessmentId, Long applicant);
 }
--- src/main/java/com/teacherfinder/assessment/application/service/AssesmentServiceImpl.java
+++ src/main/java/com/teacherfinder/assessment/application/service/AssesmentServiceImpl.java
@@ -11,12 +11,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.teacherfinder.assessment.domain.factory.TestResultFactory;
+import com.teacherfinder.assessment.domain.model.aggregate.Assessment;
 import com.teacherfinder.assessment.domain.model.entity.Question;
 import com.teacherfinder.assessment.domain.model.entity.QuestionOption;
 import com.teacherfinder.assessment.domain.model.entity.TestActivity;
+import com.teacherfinder.assessment.domain.model.entity.TestResult;
+import com.teacherfinder.assessment.domain.model.valueObject.TestResultId;
+import com.teacherfinder.assessment.domain.repository.AssesmentRepository;
 import com.teacherfinder.assessment.domain.repository.QuestionOptionRepository;
 import com.teacherfinder.assessment.domain.repository.QuestionRepository;
 import com.teacherfinder.assessment.domain.repository.TestActivityRepository;
+import com.teacherfinder.assessment.domain.repository.TestResultRepository;
 import com.teacherfinder.assessment.domain.service.AssesmentService;
 import com.teacherfinder.shared.exception.ResourceNotFoundException;
 import com.teacherfinder.shared.exception.ResourceValidationException;
@@ -25,42 +31,76 @@ import com.teacherfinder.shared.exception.ResourceValidationException;
 public class AssesmentServiceImpl implements AssesmentService {
 
     private static final String TEST = "test";
+    private static final String TEST_RESULT = "test's result";
     private static final String QUESTION = "question";
     private static final String OPTION = "option";
+    private static final String ASSESSMENT = "assessment";
     private final TestActivityRepository testRepository;
     private final QuestionRepository questionRepository;
     private final QuestionOptionRepository optionRepository;
+    private final TestResultRepository testResultRepository;
+    private final TestResultFactory testResultFactory;
+    private final AssesmentRepository assessmentRepository;
     private final Validator validator;
 
+    
+
     public AssesmentServiceImpl(TestActivityRepository testRepository, QuestionRepository questionRepository,
-            QuestionOptionRepository optionRepository, Validator validator) {
+            QuestionOptionRepository optionRepository, TestResultRepository testResultRepository,
+            TestResultFactory testResultFactory, AssesmentRepository assesmentRepository, Validator validator) {
         this.testRepository = testRepository;
         this.questionRepository = questionRepository;
         this.optionRepository = optionRepository;
+        this.testResultRepository = testResultRepository;
+        this.testResultFactory = testResultFactory;
+        this.assessmentRepository = assesmentRepository;
         this.validator = validator;
     }
 
     @Override
-    public TestActivity GetTestById(Long testId) {
-        return testRepository.findById(testId)
-                .orElseThrow(() -> new ResourceNotFoundException(TEST, testId));
+    public Assessment createAssessment(Assessment assessment) {
+        Set<ConstraintViolation<Assessment>> violations = validator.validate(assessment);
+
+        if(!violations.isEmpty())
+            throw new ResourceValidationException(ASSESSMENT, violations);
+
+        return assessmentRepository.save(assessment);
     }
 
     @Override
-    public TestActivity createTest(TestActivity test) {
+    public Assessment getAssessmentByJobOfferId(Long jobOfferId) {
+        return assessmentRepository.findByJobOfferId(jobOfferId)
+            .orElseThrow(()->new ResourceNotFoundException(ASSESSMENT, jobOfferId));
+    }
+
+    @Override
+    public TestActivity addTest(Long assessmentId,TestActivity test) {
+
+        Assessment assessment = assessmentRepository.findById(assessmentId)
+            .orElseThrow(()->new ResourceNotFoundException(ASSESSMENT, assessmentId));
+        
+        if(assessment.getTest() != null) 
+            throw new ResourceValidationException("You have already resgistered a Test");
+
         Set<ConstraintViolation<TestActivity>> violations = validator.validate(test);
         if (!violations.isEmpty())
             throw new ResourceValidationException(TEST, violations);
 
-        return testRepository.save(test);
+        return testRepository.save(test.withAssessment(assessment));
 
     }
 
     @Override
-    @Transactional
-    public ResponseEntity<String> addQuestion(Long testId, Question question) {
+    public TestActivity GetTestByAssessmentId(Long assessmentId) {
+        return testRepository.findByAssessmentAssessmentId(assessmentId)
+                .orElseThrow(() -> new ResourceNotFoundException(TEST, assessmentId));
+    }
 
-        TestActivity currentTest = validateQuestion(testId, question);
+    @Override
+    @Transactional
+    public ResponseEntity<String> addQuestion(Long assessmentId, Question question) {
+
+        TestActivity currentTest = validateQuestion(assessmentId, question);
 
         Question result = questionRepository.save(question.withTest(currentTest));
 
@@ -73,15 +113,46 @@ public class AssesmentServiceImpl implements AssesmentService {
         return new ResponseEntity<String>("Question was successfully saved", HttpStatus.OK);
     }
 
-    private TestActivity validateQuestion(Long testId, Question question){
-        TestActivity currentTest = testRepository.findById(testId)
-                .orElseThrow(() -> new ResourceNotFoundException(TEST, testId));
+    @Override
+    @Transactional
+    public TestResult calculateScore(Long assessmentId, Long applicantId ,List<Question> questions) {
+
+        TestActivity currentTest = testRepository.findByAssessmentAssessmentId(assessmentId)
+                .orElseThrow(() -> new ResourceNotFoundException(TEST, assessmentId));
+
+        Long score = 0L;
+        Boolean hasPassed = false;
+
+        for (Question question : questions) {
+            if(VerifyAnswer(question.getReponseId())) score += question.getPoints();
+        }
+
+        if(score>= currentTest.getMinimunScore()) hasPassed = true;
+
+        TestResult result = testResultFactory.createTestResult(assessmentId ,applicantId,score,currentTest, hasPassed);     
+ 
+        return testResultRepository.save(result);
+    }
+
+    private Boolean VerifyAnswer(Long optionId) {
+
+        if(optionId == 0L) return false;
+
+        QuestionOption selectedOption = optionRepository.findById(optionId)
+                .orElseThrow(() -> new ResourceNotFoundException(OPTION, optionId));
+        
+        return selectedOption.getIsCorrect();
+    }
+
+    private TestActivity validateQuestion(Long assessmentId, Question question) {
+        TestActivity currentTest = testRepository.findByAssessmentAssessmentId(assessmentId)
+                .orElseThrow(() -> new ResourceNotFoundException(TEST, assessmentId));
 
         Set<ConstraintViolation<Question>> violations = validator.validate(question);
 
         if (!violations.isEmpty())
             throw new ResourceValidationException(QUESTION, violations);
-        
+
         return currentTest;
     }
 
@@ -92,9 +163,14 @@ public class AssesmentServiceImpl implements AssesmentService {
 
             if (!violations.isEmpty())
                 throw new ResourceValidationException(OPTION, violations);
-
             optionRepository.save(option.withQuestion(question));
         }
     }
 
+    @Override
+    public TestResult getResultByTestResultId(Long assessmentId, Long applicantId) {
+        TestResultId testResultId = new TestResultId(applicantId, assessmentId);
+        return testResultRepository.findByTestResultId(testResultId)
+            .orElseThrow(()-> new ResourceNotFoundException(TEST_RESULT));
+    }
 }
