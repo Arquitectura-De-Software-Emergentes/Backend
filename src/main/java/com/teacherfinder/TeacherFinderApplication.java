b8eb712431b744329bb99176b4020f5d04fd2a43
Jesús Pacheco <jesuspacheco0219@gmail.com>
2023-06-29 03:01:11 -0500

    add swagger  domain to cors allows

--- src/main/java/com/teacherfinder/TeacherFinderApplication.java
+++ src/main/java/com/teacherfinder/TeacherFinderApplication.java
@@ -29,7 +29,7 @@ public class TeacherFinderApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/")
-						.allowedOrigins("http://localhost:4200");
+						.allowedOrigins("http://localhost:4200", "https://teacher-finder.up.railway.app");
 			}
 		};
 	}
