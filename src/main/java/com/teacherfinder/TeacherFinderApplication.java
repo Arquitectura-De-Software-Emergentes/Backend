aadfc4cac391a7982402f47ac4ec2afddc19b966
Jesús Pacheco <jesuspacheco0219@gmail.com>
2023-06-29 02:56:17 -0500

    fix cors configuration

--- src/main/java/com/teacherfinder/TeacherFinderApplication.java
+++ src/main/java/com/teacherfinder/TeacherFinderApplication.java
@@ -1,13 +1,20 @@
 package com.teacherfinder;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@OpenAPIDefinition
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 @EnableJpaAuditing
 public class TeacherFinderApplication {
 
@@ -15,4 +22,15 @@ public class TeacherFinderApplication {
 		SpringApplication.run(TeacherFinderApplication.class, args);
 	}
 
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/")
+						.allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
 }
